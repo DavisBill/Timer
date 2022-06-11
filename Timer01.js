70a1f9cc9b2f08e6597e6e67c699577a5e80c061
DavisBill <34345297+DavisBill@users.noreply.github.com>
2022-06-11 10:45:09 +0800

    Add files via upload

---
+++ Timer01.js
@@ -0,0 +1,16 @@
+
+let circle;
+let work, prepare, rest;
+function start(){
+ document.getElementById('').value;
+}
+
+function set_circle(){
+circle = document.getElementById('').value;
+}
+
+function countdown(){
+//setInterval(function(){T0=T0-1},1000);
+}
+
+countdown();
